0f23e69e1c07c8a2121b5bfdf2a194a3b77b6a6b
minecraftuser2 <144881115+minecraftuser2@users.noreply.github.com>
2026-02-15 00:48:04 +1100

    Update backend.py

--- backend.py
+++ backend.py
@@ -1,7 +1,7 @@
 import psycopg2
 import os
 
-DATABASE_URL = os.environ.get("postgresql://gamemodedb_user:ahln2WBXAacau5ipSVIOfzfly5QxBQj5@dpg-d6879nogjchc73bdu4mg-a/gamemodedb")  # Set this in Render
+DATABASE_URL = os.environ.get("DATABASE_URL")  # Set this in Render
 
 def init_db():
     conn = psycopg2.connect(DATABASE_URL)
